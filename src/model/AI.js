c4d952f957785d31ecf7953f4f8c76101f684047
Thibault Jan Beyer <hi@thibaultjanbeyer.com>
2019-05-17 20:32:51 +0200

    better guesses

--- src/model/AI.js
+++ src/model/AI.js
@@ -1,6 +1,8 @@
 const { color, uuid, int, username: usernameRnd } = require("../utils/random");
 const User = require("./User");
 
+const guessSteps = [10, 100, 1000, 10000, 100000, 1000000, 10000000];
+
 module.exports = class AI extends User {
   constructor(app) {
     super(
@@ -15,11 +17,14 @@ module.exports = class AI extends User {
       app
     );
     setTimeout(() => this.unMount(), int(0, 12 * 60 * 60 * 1000));
-    this.interval = setInterval(() => this.fakeGuess(), int(10000, 60000));
+    this.interval = setInterval(() => this.fakeGuess(), int(10000, 50000));
   }
 
   fakeGuess() {
-    if (typeof this.guess !== "number") this.guess = int(0, 2000000);
+    if (typeof this.guess !== "number") {
+      const guessRange = guessSteps[int(0, guessSteps.length - 1)];
+      this.guess = int(0, guessRange);
+    }
   }
 
   unMount() {
